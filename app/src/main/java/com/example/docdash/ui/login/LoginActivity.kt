fe44a2b0e51633875213c3b21375296d276b413b
fatih.baskin <fatih.baskin@tokeninc.com>
2023-12-25 16:32:23 +0300

    added the layout for login page

---
+++ app/src/main/java/com/example/docdash/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.docdash.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.docdash.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
