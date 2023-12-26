9dd80400647f909978a6d9ef06a109ea67b1d8ae
fatih.baskin <fatih.baskin@tokeninc.com>
2023-12-26 15:57:08 +0300

    standardized font sizes in the task pool page

--- app/src/main/java/com/example/docdash/ui/taskPool/TaskAdapter.kt
+++ app/src/main/java/com/example/docdash/ui/taskPool/TaskAdapter.kt
@@ -6,15 +6,18 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.docdash.R
-import com.example.docdash.data.TaskItem
+import com.example.docdash.data.TaskListItem
 
 class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    val dueDate: TextView = view.findViewById(R.id.taskDue)
     val taskID: TextView = view.findViewById(R.id.taskNo)
+    val dueDate: TextView = view.findViewById(R.id.taskDue)
     val taskDescription: TextView = view.findViewById(R.id.taskDescription)
+    val testDescription: TextView = view.findViewById(R.id.testDescription)
+    val patient: TextView = view.findViewById(R.id.patient)
+    val room: TextView = view.findViewById(R.id.room)
 }
 
-class TaskAdapter(private var taskList: List<TaskItem>) : RecyclerView.Adapter<ViewHolder>() {
+class TaskAdapter(private var taskList: List<TaskListItem>) : RecyclerView.Adapter<ViewHolder>() {
     override fun getItemCount(): Int {
         return taskList.size
     }
@@ -27,12 +30,15 @@ class TaskAdapter(private var taskList: List<TaskItem>) : RecyclerView.Adapter<V
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // Bind data to views
-        holder.dueDate.text = taskList[position].deadline
-        holder.taskID.text = taskList[position].entry_ID
-        holder.taskDescription.text = taskList[position].information
+        holder.taskID.text = taskList[position].taskID.toString()
+        holder.dueDate.text = taskList[position].taskDeadline
+        holder.taskDescription.text = taskList[position].taskDescription
+        holder.testDescription.text = taskList[position].testDescription
+        holder.patient.text = taskList[position].patient
+        holder.room.text = taskList[position].room
     }
 
-    fun updateTaskList(newTaskList: List<TaskItem>) {
+    fun updateTaskList(newTaskList: List<TaskListItem>) {
         this.taskList = newTaskList
         // TODO Check alternatives to notifyDataSetChanged()
         notifyDataSetChanged()
--- app/src/main/java/com/example/docdash/ui/taskPool/TaskPoolViewModel.kt
+++ app/src/main/java/com/example/docdash/ui/taskPool/TaskPoolViewModel.kt
@@ -3,18 +3,18 @@ package com.example.docdash.ui.taskPool
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.docdash.data.TaskItem
+import com.example.docdash.data.TaskListItem
 import com.example.docdash.dummyData.DummyTaskPool
 
 class TaskPoolViewModel : ViewModel() {
-    private val taskList = MutableLiveData<List<TaskItem>>()
+    private val taskList = MutableLiveData<List<TaskListItem>>()
 
     fun updateTaskList() {
         // TODO: Implement when endpoint is ready
         taskList.postValue(DummyTaskPool.dummyPool)
     }
 
-    fun getTaskList(): LiveData<List<TaskItem>> {
+    fun getTaskList(): LiveData<List<TaskListItem>> {
         return taskList
     }
 }
