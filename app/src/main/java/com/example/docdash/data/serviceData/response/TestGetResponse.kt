0b6f76f6b923cda57a60dfbd6f120c07e42ad307
fthbaskin <cs.fthbaskin@gmail.com>
2024-01-01 17:56:26 +0300

    added take task & complete task functionalities.

--- app/src/main/java/com/example/docdash/data/serviceData/response/TestGetResponse.kt
+++ app/src/main/java/com/example/docdash/data/serviceData/response/TestGetResponse.kt
@@ -2,13 +2,13 @@ package com.example.docdash.data.serviceData.response
 
 data class TestGetResponse(
     // This class holds the response of the test data.
-    val task_id: String?,
-    val information: String?,
-    val created_at: Long?,
-    val created_by: String?,
-    val status: String?,
-    val result: String?,
-    val updated_at : Long?,
-    val updated_by: String?,
-    val id: String?
+    var task_id: String?,
+    var information: String?,
+    var created_at: Long?,
+    var created_by: String?,
+    var status: String?,
+    var result: String?,
+    var updated_at : Long?,
+    var updated_by: String?,
+    var id: String?
 )
