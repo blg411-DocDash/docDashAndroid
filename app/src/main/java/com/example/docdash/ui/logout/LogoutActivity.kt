209c1d1d99b7e9031565233d1015a93874bb6225
fthbaskin <cs.fthbaskin@gmail.com>
2024-01-02 17:18:19 +0300

    Implemented the layout of logout page.

---
+++ app/src/main/java/com/example/docdash/ui/logout/LogoutActivity.kt
@@ -0,0 +1,12 @@
+package com.example.docdash.ui.logout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.docdash.R
+
+class LogoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logout)
+    }
+}
