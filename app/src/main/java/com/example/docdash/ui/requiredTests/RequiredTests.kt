3fb9ebb3eb0ab5c765ec645b30a0105b82931421
fthbaskin <cs.fthbaskin@gmail.com>
2024-01-05 23:19:47 +0300

    Fixed the issue when task is closed or open, you can edit tests.

--- app/src/main/java/com/example/docdash/ui/requiredTests/RequiredTests.kt
+++ app/src/main/java/com/example/docdash/ui/requiredTests/RequiredTests.kt
@@ -2,14 +2,9 @@ package com.example.docdash.ui.requiredTests
 
 import android.content.Intent
 import android.util.Log
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,10 +12,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -30,15 +23,17 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -47,21 +42,12 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.example.docdash.R
 import com.example.docdash.data.serviceData.response.TestGetResponse
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.docdash.ui.taskDetails.TaskDetailsActivity
-import com.example.docdash.ui.taskDetails.TaskDetailsViewModel
 import com.example.docdash.utils.DateTimeHandler
 import com.google.gson.Gson
 
@@ -234,6 +220,7 @@ fun EditableTextField(item: TestGetResponse, viewModel: RequiredTestsViewModel) 
     val itemText = item.result
     var text by remember(itemText) { mutableStateOf(itemText ?: "Write Results...") }
     var isEditing by remember { mutableStateOf(true) }
+    val taskStatus = viewModel.taskDetailsLiveData.value?.status
 
     Row(
         modifier = Modifier
@@ -269,12 +256,20 @@ fun EditableTextField(item: TestGetResponse, viewModel: RequiredTestsViewModel) 
         Spacer(modifier = Modifier.width(5.dp))
         Button(
             onClick = {
-                isEditing = false
-
-                viewModel.updateTestResult(id = item.id, result = text)
+                if (taskStatus == "in progress") {
+                    isEditing = false
+                    viewModel.updateTestResult(id = item.id, result = text)
+                }
             },
              colors = ButtonDefaults.buttonColors(
-                containerColor = colorResource(id = R.color.dark_blue)
+                 containerColor = when (taskStatus) {
+                     "in progress" -> {
+                         colorResource(id = R.color.light_green)
+                     }
+                     else -> {
+                         colorResource(id = R.color.light_gray)
+                     }
+                 }
             )
         ) {
             Text(
